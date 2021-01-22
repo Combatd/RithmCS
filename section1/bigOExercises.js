5cbf3d9e8e51c1653d7bbbd4e50b2569f4e3d914
Combatd <combatdstar@gmail.com>
2021-01-22 07:21:30 -0800

    Study Big O

---
+++ section1/bigOExercises.js
@@ -0,0 +1,49 @@
+// 1. O(n) Linear
+
+function logUpTo(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+// 2. O(1) Constant
+
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// 3. O(n) Constant
+
+function logAtLeast10(n) {
+    for (var i = 1; i <= Math.max(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// 4. O(logn) linear log - ends up less than n
+
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+// 5. O(n^2) - Runs a set of iterations up to n at each iteration of n
+
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
