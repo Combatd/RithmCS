32d3ada4a9b6f75447020683595fcae3f16b0ade
Combatd <combatdstar@gmail.com>
2021-01-22 14:07:54 -0800

    Define #contains

--- section1/recursionExercises.js
+++ section1/recursionExercises.js
@@ -13,3 +13,39 @@ function productOfArray(numbers) {
 
 console.log(productOfArray([1,2,3])) // 6
 console.log(productOfArray([1,2,3,10])) // 60
+
+/*
+Write a function called contains that searches for a value in a nested object. 
+It returns true if the object contains that value.
+*/
+
+
+function contains(nestedObject, value) {
+    const allKeys = Object.keys(nestedObject);
+    // base case, for loop through the object's keys
+    for(let i = 0; i < allKeys.length; i++) {
+        if (nestedObject[allKeys[i]] === value) return true; 
+        // recursive invocation, will return the nested objects key
+        if (typeof nestedObject[allKeys[i]] === 'object') {
+            return contains(nestedObject[allKeys[i]], value);
+        }
+    }
+    return false; // when we get through every single key
+}
+
+let bigObj = {
+    data: {
+      info: {
+        stuff: {
+          thing: {
+            moreStuff: {
+              magicNumber: 44,
+              something: 'foo2'
+            }
+          }
+        }
+      }
+    }
+  };
+console.log(contains(bigObj, 44)) // true
+console.log(contains(bigObj, "foo")) // false
