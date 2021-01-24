a824d7f5dab6e71133e15a5a5daa43d0537e19e0
Combatd <combatdstar@gmail.com>
2021-01-24 14:30:31 -0800

    Define #selectionSort

--- section2/sorting.js
+++ section2/sorting.js
@@ -51,3 +51,34 @@ function bubbleSort(arr) {
 // console.log(bubbleSort(sorted));
 // console.log(bubbleSort(unsortedNegative));
 // console.log(bubbleSort(sortedNegative));
+
+/*
+#selectionSort
+
+Implement selection sort. Your function should accept an array and return an array of sorted values.
+*/
+
+function selectionSort(arr) {
+    let copyArr = [...arr]; // functional programming retains original state
+    let currentIndex = 1; // Start by picking the second element in the array (we will assume the first element is the start of the "sorted" portion)
+
+    while (currentIndex < copyArr.length) {
+        for (let i = 0; i < currentIndex; i++) {
+            // Now compare the second element with the one before it and swap if necessary.
+            if (copyArr[currentIndex] < copyArr[i]) {
+                let currentElement = copyArr[i];
+                copyArr[i] = copyArr[currentIndex];
+                copyArr[currentIndex] = currentElement;
+            }
+        }
+        // Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.
+        currentIndex += 1;
+    }
+    // Repeat until the array is sorted.
+    return copyArr;
+}
+
+console.log(selectionSort(unsorted));
+console.log(selectionSort(sorted));
+console.log(selectionSort(unsortedNegative));
+console.log(selectionSort(sortedNegative));
