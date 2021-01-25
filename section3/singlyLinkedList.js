b9dc9e4b9fe9cb67ca7a04898e5fc18ab00dbc0a
Combatd <combatdstar@gmail.com>
2021-01-25 07:07:42 -0800

    Create singlyLinkedList.js

---
+++ section3/singlyLinkedList.js
@@ -0,0 +1,79 @@
+/*
+Node: each element in the list is called a node
+Head: the first element in the list
+Tail: the last element in the list
+Next: usually referring to the pointer to the next node in the list
+Previous: in a doubly linked list, the pointer to the previous element in the list (more on this later)
+*/
+
+function Node(val){
+    this.val = val;
+    this.next = null;
+}
+
+function SinglyLinkedList(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}
+
+/*
+#push
+This function should add a node to the end of the SinglyLinkedList. 
+It should return the list so that the method can be chained.
+/*
+
+
+/*
+#pop
+This function should remove a node at the end of the SinglyLinkedList. 
+It should return the node removed.
+*/
+
+
+/*
+unshift
+This function should add a node to the beginning of the SinglyLinkedList. 
+It should return the list so that the method can be chained.
+*/
+
+
+/*
+#shift
+This function should remove a node at the beginning of the SinglyLinkedList. 
+It should return the node removed.
+*/
+
+
+/*
+#set
+This function should update the value of a node at a given index of the SinglyLinkedList. 
+It should return true if the node is updated successfully, or false if an invalid index is passed in.
+*/
+
+
+/*
+#_get
+This internal/helper function should find a node at a specified index in a SinglyLinkedList. 
+It should return the found node.
+*/
+
+/*
+#_insert
+This internal/helper function should insert a node at a specified index in a SinglyLinkedList. 
+It should return the new length of the SinglyLinkedList.
+*/
+
+
+/*
+#remove
+This function should remove a node at a specified index in a SinglyLinkedList. 
+It should return the removed node.
+*/
+
+
+/*
+#reverse
+This function should reverse all of the nodes in a SinglyLinkedList. 
+It should return the reversed SinglyLinkedList.
+*/
