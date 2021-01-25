112cb42c8086aa91b559078e2ac091f9f43b91aa
Combatd <combatdstar@gmail.com>
2021-01-24 17:01:16 -0800

    Define #selectionSort

--- section2/sorting.js
+++ section2/sorting.js
@@ -28,7 +28,7 @@ Your function should accept an array and return an array of sorted values.
 */
 
 function bubbleSort(arr) {
-    let copyArr = arr.slice(0); // functional programming retains original state
+    const copyArr = arr.slice(0); // functional programming retains original state
     for (let i = 0; i < copyArr.length; i++) {
         // For each element in the array, compare it with the next element (the element to the right).
         
@@ -59,7 +59,7 @@ Implement insertion sort. Your function should accept an array and return an arr
 */
 
 function insertionSort(arr) {
-    let copyArr = [...arr]; // functional programming retains original state
+    const copyArr = [...arr]; // functional programming retains original state
     let currentIndex = 1; // Start by picking the second element in the array (we will assume the first element is the start of the "sorted" portion)
 
     while (currentIndex < copyArr.length) {
@@ -78,7 +78,44 @@ function insertionSort(arr) {
     return copyArr;
 }
 
-console.log(insertionSort(unsorted));
-console.log(insertionSort(sorted));
-console.log(insertionSort(unsortedNegative));
-console.log(insertionSort(sortedNegative));
+// console.log(insertionSort(unsorted));
+// console.log(insertionSort(sorted));
+// console.log(insertionSort(unsortedNegative));
+// console.log(insertionSort(sortedNegative));
+
+/*
+#selectionSort
+Implement selection sort. 
+Your function should accept an array and return an array of sorted values.
+*/
+
+function selectionSort(arr) {
+    const copyArr = arr.slice(0); // functional programming maintains original state
+
+    for (let i = 0; i < copyArr.length; i++) {
+        let minimum = i;
+        // Assign the first element to be the smallest value (this is called the minimum). It does not matter right now if this actually the smallest value in the array.
+        // Compare this item to the next item in the array until you find a smaller number.
+        for (let j = i + 1; j < copyArr.length; j++) {
+            // If a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
+            if (copyArr[minimum] > copyArr[j]) {
+                minimum = j;
+            }
+        }
+        // If the "minimum" is not the value (index) you initially began with, swap the two values. You will now see that the beginning of the array is in the correct order (similar to how after the first iteration of bubble sort, we know the rightmost element is in its correct place).
+        if (minimum != i) {
+            let previousMinimumValue = copyArr[i]; // temporary holding variable
+            copyArr[i] = copyArr[minimum]
+            copyArr[minimum] = previousMinimumValue;
+        }
+        // reset the minimum (happens at the beginning of next loop)
+        
+        // Repeat this with the next element until the array is sorted.
+    }
+    return copyArr;
+}
+
+// console.log(selectionSort(unsorted));
+// console.log(selectionSort(sorted));
+// console.log(selectionSort(unsortedNegative));
+// console.log(selectionSort(sortedNegative));
