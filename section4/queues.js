90381fb83e9b6053e397473909d33a63f3ec43c5
Combatd <combatdstar@gmail.com>
2021-04-03 14:16:39 -0700

    Define Queue#enqueue

--- section4/queues.js
+++ section4/queues.js
@@ -17,3 +17,28 @@ function Queue() {
     this.first = null;
     this.last = null;
 }
+
+/*
+#enqueue
+This function adds the value to the end of the queue. 
+This should be an O(1) operation and return the size of the queue.
+*/
+
+Queue.prototype.enqueue = function(val) {
+    this.storage.push(val);
+    this.size += 1;
+    // we need to set Queue.first and Queue.last property
+    if(this.size === 1) {
+        this.first = this.storage[0];
+    } else {
+        this.last = this.storage[this.size - 1]; // last Node in Queue
+    }
+}
+
+let q = new Queue();;
+
+q.enqueue(5);
+q.enqueue(10);
+q.enqueue(15);
+
+console.log('The Queue named q: ', q);
